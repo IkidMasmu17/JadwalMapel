a5751fc83d78500ccbea485e3c42c933fbe464c9
= <=>
2026-01-06 07:50:15 +0700

    fix: Remove dark mode from main layout and sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-light-primary elevation-4">
   <!-- Brand Logo -->
   <a href="index3.html" class="brand-link">
     <span class="brand-text font-weight-light">Sistem Mapel</span>
